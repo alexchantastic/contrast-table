ac8d4b86cfbc26228eb885d0d976dfd9f26c3f63
Alex Chan <alexchantastic@gmail.com>
2018-11-24 13:47:25 -0800

    Add URL palette

--- src/App.js
+++ src/App.js
@@ -7,14 +7,58 @@ import Table from './components/Table/Table';
 import './App.scss';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.palette = ['#zzz', 'thisisnotavalidcolorvalue', 'rgb(255, 255, 255)', '#fff', '#000', '#1a74ba', '#b4d455', 'red', 'cornflowerblue', 'lightgoldenrodyellow', 'rgba(43, 23, 55, 0.5)', '#aaa', '#bbb', '#ccc', '#ddd', '#eee'];
-
-    this.palette = this.palette.filter((swatch) => {
+  cleanPalette = (palette) => {
+    return palette.filter((swatch) => {
       return color(swatch) || isNamedCssColor(swatch);
     });
+  }
+
+  componentWillMount() {
+    let urlPalette = window.location.search ? window.location.href
+      .replace(window.location.origin + '/?', '') // Trim URL
+      .replace(/\s/g, '') // Trim whitespace
+      .replace(/%20/g, '') // Trim escaped whitespace
+      .replace(/(,)(\d)/g, '|$2') // Use '|' as a placeholder for ',' in rgb and rgba definitions
+      .split(',') : []; // Create array of values
+
+    // Replace placeholder '|' with ','
+    urlPalette.forEach((swatch, index) => {
+      urlPalette[index] = swatch.replace(/\|/g, ',');
+    });
+
+    urlPalette = this.cleanPalette(urlPalette);
+
+    if (urlPalette.length) {
+      this.palette = urlPalette
+    } else {
+      this.palette = [
+        'white',
+        'black',
+        'rgb(100, 100, 100)',
+        'rgba(200, 200, 200, 0.5)',
+        '#ef5350',
+        '#ec407a',
+        '#ab47bc',
+        '#7e57c2',
+        '#5c6bc0',
+        '#42a5f5',
+        '#29b6f6',
+        '#26c6da',
+        '#26a69a',
+        '#66bb6a',
+        '#9ccc65',
+        '#d4e157',
+        '#ffee58',
+        '#ffca28',
+        '#ffa726',
+        '#ff7043',
+        '#8d6e63',
+        '#bdbdbd',
+        '#78909c',
+      ];
+    }
+
+    console.log(this.palette);
   }
 
   render() {
