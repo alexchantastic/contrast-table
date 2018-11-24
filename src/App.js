6abecf023427d82747776d087ea5659e1ab1e098
Alex Chan <alexchantastic@gmail.com>
2018-11-23 21:21:30 -0800

    Add palette validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import color from 'rgb';
+import isNamedCssColor from 'is-named-css-color';
 
 import Table from './components/Table/Table';
 
 import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.palette = ['#zzz', 'thisisnotavalidcolorvalue', 'rgb(255, 255, 255)', '#fff', '#000', '#1a74ba', '#b4d455', 'red', 'cornflowerblue', 'lightgoldenrodyellow', 'rgba(43, 23, 55, 0.5)', '#aaa', '#bbb', '#ccc', '#ddd', '#eee'];
+
+    this.palette = this.palette.filter((swatch) => {
+      return color(swatch) || isNamedCssColor(swatch);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Table palette={['#fff', '#000', '#1a74ba', '#b4d455', 'red', 'cornflowerblue', 'lightgoldenrodyellow', 'rgba(43, 23, 55, 0.5)', '#aaa', '#bbb', '#ccc', '#ddd', '#eee']} />
+        <Table palette={this.palette} />
       </div>
     );
   }
