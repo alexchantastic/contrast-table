099efa39214fae0ee254bcb3dc14dfeae2cbc93b
Alex Chan <alexchantastic@gmail.com>
2018-11-23 20:38:31 -0800

    Add sticky table headers

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Table palette={['#fff', '#000', '#1a74ba', '#b4d455', 'red', 'cornflowerblue', 'lightgoldenrodyellow', 'rgba(43, 23, 55, 0.5)']} />
+        <Table palette={['#fff', '#000', '#1a74ba', '#b4d455', 'red', 'cornflowerblue', 'lightgoldenrodyellow', 'rgba(43, 23, 55, 0.5)', '#aaa', '#bbb', '#ccc', '#ddd', '#eee']} />
       </div>
     );
   }
